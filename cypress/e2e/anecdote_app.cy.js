ae4ced0679c1eaaef3f273781cb5e5cb52b32d21
Sampo Hakola <sampo.hakola@helsinki.fi>
2023-08-01 17:02:13 +0300

    e2e test

--- cypress/e2e/anecdote_app.cy.js
+++ cypress/e2e/anecdote_app.cy.js
@@ -1,5 +1,16 @@
-describe('template spec', () => {
+describe('anecdotes spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
+  })
+
+  it('home opens', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('Anecdotes')
+  })
+
+  it('home opens', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('vote').click()
+    cy.contains('4')
   })
 })
